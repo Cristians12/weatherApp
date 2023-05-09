f86f6f33a30d0a4936191b32835d33f7ecb6bd31
Cristians12 <cristi_s007@yahoo.ro>
2023-05-09 21:50:07 +0300

    weather app

---
+++ script.js
@@ -0,0 +1,59 @@
+const apikey = "094645a3249c492fddf1e98f5b0f8c86";
+
+const weatherDataEl = document.getElementById("weather-data");
+
+const cityInputEl = document.getElementById("city-input");
+
+const formEl = document.querySelector("form");
+
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const cityValue = cityInputEl.value;
+  getWeatherData(cityValue);
+});
+
+async function getWeatherData(cityValue) {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityValue}&appid=${apikey}&units=metric`
+    );
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+
+    const temp = Math.round(data.main.temp);
+
+    const description = data.weather[0].description;
+
+    const icon = data.weather[0].icon;
+
+    const details = [
+      `Feels like: ${Math.round(data.main.feels_like)}`,
+      `Humidity: ${data.main.humidity}%`,
+      `Wind speed: ${data.wind.speed} m/s`,
+    ];
+
+    weatherDataEl.querySelector(".icon").innerHTML = `
+      <img
+        src="http://openweathermap.org/img/wn/${icon}.png"
+        alt="Weather Icon"
+      />
+    `;
+    weatherDataEl.querySelector(".temperature").textContent = `${temp}°C`;
+
+    weatherDataEl.querySelector(".description").textContent = description;
+
+    weatherDataEl.querySelector(".details").innerHTML = details
+      .map((detail) => `<div>${detail}</div>`)
+      .join("");
+  } catch (error) {
+    weatherDataEl.querySelector(".icon").innerHTML = "";
+    weatherDataEl.querySelector(".temperature").textContent = "";
+
+    weatherDataEl.querySelector(".description").textContent =
+      "An error happened, please try again";
+
+    weatherDataEl.querySelector(".details").innerHTML = "";
+  }
+}
